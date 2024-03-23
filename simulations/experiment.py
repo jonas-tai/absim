20867e73e70464fb344d6801027912b700d607ba
Haley <hly_li@outlook.com>
2024-03-22 21:10:37 -0700

    Annotate arguments

--- simulations/experiment.py
+++ simulations/experiment.py
@@ -129,7 +129,7 @@ def runExperiment(args):
             mup = muUpdater.MuUpdater(serv, args.intervalParam,
                                       args.serviceTime,
                                       args.timeVaryingDrift)
-            Simulation.process(mup.run(),at=0.0)
+            Simulation.process(mup.run(), at=0.0)
             servers.append(serv)
     else:
         print("Unknown experiment scenario")
@@ -215,8 +215,6 @@ def runExperiment(args):
     if not exp_path.exists():
         exp_path.mkdir(parents=True, exist_ok=True)
 
-
-
     pendingRequestsFD = open("../%s/%s_PendingRequests" %
                              (args.logFolder,
                               args.expPrefix), 'w')
@@ -285,71 +283,89 @@ def runExperiment(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Absinthe sim.')
     parser.add_argument('--numClients', nargs='?',
-                        type=int, default=1)
+                        type=int, default=1, help='Number of clients')
     parser.add_argument('--numServers', nargs='?',
-                        type=int, default=5)
+                        type=int, default=5, help='Number of servers')
     parser.add_argument('--numWorkload', nargs='?',
-                        type=int, default=1)
+                        type=int, default=1, help='Number of workload generators. Seems to distribute the '
+                                                  'tasks out to different clients.')
     parser.add_argument('--serverConcurrency', nargs='?',
-                        type=int, default=1)
+                        type=int, default=1, help='Amount of resources per server.')
     parser.add_argument('--serviceTime', nargs='?',
-                        type=float, default=1)
+                        type=float, default=1, help='Mean? service time per server')
     parser.add_argument('--workloadModel', nargs='?',
-                        type=str, default="poisson")
+                        type=str, default="poisson", help='Arrival model of requests from client')
     parser.add_argument('--utilization', nargs='?',
-                        type=float, default=0.90)
+                        type=float, default=0.90, help='Arrival rate of requests')
     parser.add_argument('--serviceTimeModel', nargs='?',
-                        type=str, default="random.expovariate")
+                        type=str, default="random.expovariate", help='Distribution of service time on server')
     parser.add_argument('--replicationFactor', nargs='?',
-                        type=int, default=1)
+                        type=int, default=1, help='Replication factor (# of choices)')
     parser.add_argument('--selectionStrategy', nargs='?',
-                        type=str, default="pending")
+                        type=str, default="expDelay", help='Policy to use for replica selection')
     parser.add_argument('--shadowReadRatio', nargs='?',
-                        type=float, default=0.10)
+                        type=float, default=0.10, help='Controls the probability of sending a shadow read '
+                                                       '(idk exactly what this is, it seems to be a function '
+                                                       'that sends requests out to non-chosen servers to force '
+                                                       'an update.')
     parser.add_argument('--rateInterval', nargs='?',
-                        type=int, default=10)
+                        type=int, default=10, help='Unclear what this one does')
     parser.add_argument('--cubicC', nargs='?',
-                        type=float, default=0.000004)
+                        type=float, default=0.000004, help='Controls sending rate of client (Called gamma in paper)')
     parser.add_argument('--cubicSmax', nargs='?',
-                        type=float, default=10)
+                        type=float, default=10, help='Controls sending rate of client. ')
     parser.add_argument('--cubicBeta', nargs='?',
-                        type=float, default=0.2)
+                        type=float, default=0.2, help='Controls sending rate of client')
     parser.add_argument('--hysterisisFactor', nargs='?',
-                        type=float, default=2)
+                        type=float, default=2, help='Hysteresis period before another rate change')
     parser.add_argument('--backpressure', action='store_true',
-                        default=False)
+                        default=False, help='Adds backpressure mode which waits once rate limits are reached')
     parser.add_argument('--accessPattern', nargs='?',
-                        type=str, default="uniform")
+                        type=str, default="uniform", help='Key access pattern of requests, e.g., zipfian will cause '
+                                                          'requests to desire a subset of replica sets')
     parser.add_argument('--nwLatencyBase', nargs='?',
-                        type=float, default=0.960)
+                        type=float, default=0.960, help='Seems to be the time it takes to deliver requests?')
     parser.add_argument('--nwLatencyMu', nargs='?',
-                        type=float, default=0.040)
+                        type=float, default=0.040, help='Seems to be the time it takes to deliver requests?')
     parser.add_argument('--nwLatencySigma', nargs='?',
-                        type=float, default=0.0)
+                        type=float, default=0.0, help='Seems to be the time it takes to deliver requests?')
     parser.add_argument('--expPrefix', nargs='?',
                         type=str, default="0")
     parser.add_argument('--seed', nargs='?',
                         type=int, default=25072014)
     parser.add_argument('--simulationDuration', nargs='?',
-                        type=int, default=500)
+                        type=int, default=500, help='Time that experiment takes, '
+                                                    'note that if this is too low and numRequests is too high, '
+                                                    'it will error')
     parser.add_argument('--numRequests', nargs='?',
-                        type=int, default=100)
+                        type=int, default=100, help='Number of requests')
     parser.add_argument('--logFolder', nargs='?',
                         type=str, default="logs")
     parser.add_argument('--expScenario', nargs='?',
-                        type=str, default="base")
+                        type=str, default="base", help='Defines some scenarios for experiments such as \n'
+                                                       '[base] - default setting\n'
+                                                       '[multipleServiceTimeServers] - increasing mean service time '
+                                                       'based on server index\n'
+                                                       '[heterogenousStaticServiceTimeScenario] - '
+                                                       'fraction of servers are slower\n'
+                                                       '[timeVaryingServiceTimeServers] - servers change service times')
     parser.add_argument('--demandSkew', nargs='?',
-                        type=float, default=0)
+                        type=float, default=0, help='Skews clients such that some clients send many'
+                                                    ' more requests than others')
     parser.add_argument('--highDemandFraction', nargs='?',
-                        type=float, default=0)
+                        type=float, default=0, help='Fraction of the high demand clients')
     parser.add_argument('--slowServerFraction', nargs='?',
-                        type=float, default=0)
+                        type=float, default=0, help='Fraction of slow servers '
+                                                    '(expScenario=heterogenousStaticServiceTimeScenario)')
     parser.add_argument('--slowServerSlowness', nargs='?',
-                        type=float, default=0)
+                        type=float, default=0, help='How slow those slowed servers are '
+                                                    '(expScenario=heterogenousStaticServiceTimeScenario)')
     parser.add_argument('--intervalParam', nargs='?',
-                        type=float, default=0.0)
+                        type=float, default=0.0, help='Interval between which server service times change '
+                                                      '(expScenario=timeVaryingServiceTimeServers)')
     parser.add_argument('--timeVaryingDrift', nargs='?',
-                        type=float, default=0.0)
+                        type=float, default=0.0, help='How much service times change '
+                                                      '(expScenario=timeVaryingServiceTimeServers)')
     args = parser.parse_args()
 
     runExperiment(args)
