Fix normalization of data and add export of feature data

simulations/state.py
@@ -83,7 +83,7 @@ class StateParser:
 
         return torch.tensor([state_features], dtype=torch.float32)
 
-    def state_to_tensor(self, state: State, server_permutation: List[int] | None = None, degree: int = None) -> torch.Tensor:
+    def state_to_tensor(self, state: State, server_permutation: List[int] | None = None, degree: int | None = None) -> torch.Tensor:
         if server_permutation is None:
             server_permutation = [i for i in range(len(state.node_states))]
 

simulations/training/offline_model_trainer.py
@@ -205,14 +205,11 @@ class OfflineTrainer:
         # Make sure that data is aligned properly
         assert len(action_reward_policy_df) == len(state_df) and len(state_df) == len(next_state_df)
 
-        reward_mean = torch.tensor([action_reward_policy_df['reward'].mean()], dtype=torch.float32, device=self.device)
-        reward_std = torch.tensor([action_reward_policy_df['reward'].std()], dtype=torch.float32, device=self.device)
+        self.reward_mean = torch.tensor([action_reward_policy_df['reward'].mean()], dtype=torch.float32, device=self.device)
+        self.reward_std = torch.tensor([action_reward_policy_df['reward'].std()], dtype=torch.float32, device=self.device)
 
-        feature_mean = torch.tensor(state_df.mean(), dtype=torch.float32, device=self.device)
-        feature_std = torch.tensor(state_df.std(), dtype=torch.float32, device=self.device)
-
-        self.norm_stats = NormStats(reward_mean=reward_mean, reward_std=reward_std,
-                                    feature_mean=feature_mean, feature_std=feature_std)
+        self.feature_mean = torch.tensor(state_df.mean(), dtype=torch.float32, device=self.device)
+        self.feature_std = torch.tensor(state_df.std(), dtype=torch.float32, device=self.device)
 
         self.expert_memory = ReplayMemory(max_size=len(state_df),
                                           always_use_newest=self.replay_always_use_newest)
@@ -222,7 +219,6 @@ class OfflineTrainer:
             reward = torch.tensor([[action_reward_policy_row.reward]], dtype=torch.float32, device=self.device)
             state = torch.tensor([state_row], dtype=torch.float32, device=self.device)
             next_state = torch.tensor([next_state_row], dtype=torch.float32, device=self.device)
-
             transition = Transition(state=state, action=action, next_state=next_state, reward=reward)
             norm_transition = self.normalize_transition(transition=transition)
 

simulations/training/training_data_collector.py
@@ -163,14 +163,14 @@ class TrainingDataCollector:
         # Make sure that data is aligned properly
         assert len(action_reward_policy_df) == len(state_df) and len(state_df) == len(next_state_df)
 
-        reward_mean = torch.tensor([action_reward_policy_df['reward'].mean()], dtype=torch.float32, device=self.device)
-        reward_std = torch.tensor([action_reward_policy_df['reward'].std()], dtype=torch.float32, device=self.device)
+        self.offline_trainer.reward_mean = torch.tensor([action_reward_policy_df['reward'].mean()], dtype=torch.float32, device=self.device)
+        self.offline_trainer.reward_std = torch.tensor([action_reward_policy_df['reward'].std()], dtype=torch.float32, device=self.device)
 
-        feature_mean = torch.tensor(state_df.mean(), dtype=torch.float32, device=self.device)
-        feature_std = torch.tensor(state_df.std(), dtype=torch.float32, device=self.device)
+        self.offline_trainer.feature_mean = torch.tensor(state_df.mean(), dtype=torch.float32, device=self.device)
+        self.offline_trainer.feature_std = torch.tensor(state_df.std(), dtype=torch.float32, device=self.device)
 
-        self.offline_trainer.norm_stats = NormStats(reward_mean=reward_mean, reward_std=reward_std,
-                                                    feature_mean=feature_mean, feature_std=feature_std)
+        # self.norm_stats = NormStats(reward_mean=reward_mean, reward_std=reward_std,
+        #                                             feature_mean=feature_mean, feature_std=feature_std)
 
         self.offline_trainer.expert_memory = ReplayMemory(max_size=len(state_df),
                                                           always_use_newest=self.offline_trainer.replay_always_use_newest)
@@ -183,7 +183,7 @@ class TrainingDataCollector:
 
             transition = Transition(state=state, action=action, next_state=next_state, reward=reward)
             norm_transition = self.offline_trainer.normalize_transition(transition=transition)
-
+            
             # Store the normalized transition in memory
             self.offline_trainer.expert_memory.push_transition(transition=norm_transition)
 